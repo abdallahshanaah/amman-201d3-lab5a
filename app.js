207be75da93ef45b67a73fcc736e5f8325791b9b
abdallahshanaah <abdallah_m_sh@yahoo.com>
2020-02-22 17:32:29 +0200

    q3 answer

--- app.js
+++ app.js
@@ -65,14 +65,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 
 
-// function sumAndMultiply(a, b, c) { //eslint-disable-line
-//     var s1=sum(4,7);
-//     var s2=sum(s1[0],5);
-//     var m1=multiply(4,7);
-//     var m2 = multiply(m1[0],5);
-// return[s2[0],m2[0],`${a} and ${b} and ${c} sum to ${s2[0]}.`,`The product of ${a} and ${b} and ${c} is ${m2[0]}.`]
-// }
-// testSumAndMultiply(4,7,5);
+function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var s1=sum(4,7);
+    var s2=sum(s1[0],5);
+    var m1=multiply(4,7);
+    var m2 = multiply(m1[0],5);
+return[s2[0],m2[0],`${a} and ${b} and ${c} sum to ${s2[0]}.`,`The product of ${a} and ${b} and ${c} is ${m2[0]}.`]
+}
+testSumAndMultiply(4,7,5);
 
 
 
